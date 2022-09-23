45bcfc2d3575b41ce05a09b9c0fc587557a63a94
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-09-23 11:47:54 -0300

    CRUD Elastic

--- API/Controllers/PattiesController.cs
+++ API/Controllers/PattiesController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,38 +14,132 @@ namespace API.Controllers
     [ApiController]
     public class PattiesController : ControllerBase
     {
+        private readonly IElasticClient _elasticClient;
+
+        public PattiesController(IElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+        }
         // GET: api/<PattiesController>
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> GetAll()
         {
-            return Ok();
+            var searchResponse = _elasticClient.Search<Patty>(s => s
+                                .Index("atenea-tv-patty"));
+
+            var people = searchResponse.Documents;
+
+            var result = people?.ToList();
+
+            return Ok(result);
         }
 
         // GET api/<PattiesController>/5
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(long id)
         {
-            return Ok();
+            var result = _elasticClient.Get<Patty>(id);
+
+            return Ok(result.Source);
         }
 
         // POST api/<PattiesController>
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] CreatePatty request)
         {
-            return Created("NewPatty", "");
+            bool status;
+            var random = new Random();
+            long guid = random.Next(99, 999);
+
+            var patty = new Patty()
+            {
+                Id = guid,
+                Name = request.Name,
+                Stock = request.Stock,
+                DateOfElaboration = DateTime.Now
+            };
+
+            var response = _elasticClient.IndexDocument(patty);
+
+            if (response.IsValid)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+            if (status)
+            {
+                return Created("NewPatty", patty);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
         }
 
         // PUT api/<PattiesController>/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(long id, [FromBody] UpdatePatty request)
         {
-            return Ok();
+            bool status;
+
+            var patty = new Patty()
+            {
+                Id = id,
+                Name = request.Name,
+                Stock = request.Stock,
+                DateOfElaboration = DateTime.MinValue
+            };
+
+            var response = _elasticClient.Update<Patty, Patty>(patty.Id, d => d
+                            .Index("atenea-tv-patty")
+                            .Doc(patty));
+
+            if (response.IsValid)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+
+            if (status)
+            {
+                return Ok(patty);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
         }
 
         // DELETE api/<PattiesController>/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
+            bool status;
+
+            var response = _elasticClient.Delete<Patty>(id, d => d
+                            .Index("atenea-tv-patty"));
+
+            if (response.IsValid)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+            status.ToString();
+
             return NoContent();
         }
     }
