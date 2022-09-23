07acc87c03e873aa344595002bfbd301798c053b
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-09-23 11:18:21 -0300

    Crear indices, configuracion inicial elastic search

---
+++ API/ServiceExtensions/ElasticsearchExtension.cs
@@ -0,0 +1,59 @@
+﻿using API.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using System;
+
+namespace API.ServiceExtensions
+{
+    public static class ElasticsearchExtensions
+    {
+        public static string PattyIndex = "atenea-tv-patty";
+
+        public static void AddElasticsearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            var url = configuration["elasticsearch:url"];
+            var user = configuration["elasticsearch:user"];
+            var password = configuration["elasticsearch:password"];
+            var defaultIndex = configuration["elasticsearch:index"];
+
+            var settings = new ConnectionSettings(new Uri(url))
+                .DefaultIndex(defaultIndex)
+                .BasicAuthentication(user, password);
+
+            AddDefaultMappings(settings);
+
+            var client = new ElasticClient(settings);
+
+            services.AddSingleton<IElasticClient>(client);
+
+            CreateIndex(client, defaultIndex);
+            CreatePattyIndex(client, PattyIndex);
+        }
+
+        #region Private Methods
+
+        private static void AddDefaultMappings(ConnectionSettings settings)
+        {
+            settings.DefaultMappingFor<Patty>(t => t.IndexName(PattyIndex));
+        }
+
+        private static void CreateIndex(IElasticClient client, string indexName)
+        {
+            var response = client.Indices.Create(indexName,
+                         index => index.Map<Man>(
+                             x => x.AutoMap()
+                         ));
+        }
+
+        private static void CreatePattyIndex(IElasticClient client, string indexName)
+        {
+            var response = client.Indices.Create(indexName,
+                index => index.Map<Patty>(
+                    x => x.AutoMap()
+                ));
+        }
+
+        #endregion
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using API.ServiceExtensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,9 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            // Elasticsearch
+            services.AddElasticsearch(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
