8dbe8fe8d1bb7532872bd530a3df5350abde66fe
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-11-18 14:04:58 -0300

    +++++++++++++++

---
+++ API/Controllers/_TestController.cs
@@ -0,0 +1,15 @@
+﻿using API.Models.Tests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class _TestController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] RequestBody request)
+        {
+        }        
+    }
+}
---
+++ API/Models/Tests/RequestBody.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace API.Models.Tests
+{
+    public class RequestBody
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
