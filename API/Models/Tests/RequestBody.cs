af424277ed1b4cb9f70acc35069aa78b41c3245c
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-11-18 14:42:47 -0300

    key words

--- API/Models/Tests/RequestBody.cs
+++ API/Models/Tests/RequestBody.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace API.Models.Tests
 {
@@ -9,6 +10,8 @@ namespace API.Models.Tests
         public int Stock { get; set; }
         public DateTime DateFrom { get; set; }
         public DateTime DateTo { get; set; }
+
+        public List<string> KeyWords { get; set; } = new List<string>();
 
         private int _size;
 
