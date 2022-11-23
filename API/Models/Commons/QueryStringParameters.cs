23f7fe7205cd61726116b907a4df35c9738e45fa
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-11-23 09:15:07 -0500

    formato correcto

--- API/Models/Commons/QueryStringParameters.cs
+++ API/Models/Commons/QueryStringParameters.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Models.Commons
+﻿using System;
+
+namespace API.Models.Commons
 {
     public class QueryStringParameters
     {
@@ -17,8 +19,50 @@
         public string FilterByGuid { get; set; }
         public string FilterByName { get; set; }
         public string FilterByStock { get; set; }
-        public string FilterByDateOfExpiration { get; set; }
-        public string FilterDateFrom { get; set; }
-        public string FilterDateTo { get; set; }
+
+        /*
+2022-01-01T00:00:00Z
+2022-12-31T23:59:59Z
+
+(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+         */
+        private string _filterByDateOfExpiration;
+        private string _filterDateFrom;
+        private string _filterDateTo;
+
+        public string FilterByDateOfExpiration
+        {
+            get
+            {
+                var dateExpiration = Convert.ToDateTime(_filterByDateOfExpiration);
+
+                return dateExpiration.ToString("yyyy-MM-ddT00:00:00Z");
+            }
+            set { _filterByDateOfExpiration = value; }
+        }
+
+        public string FilterDateFrom
+        {
+            get
+            {
+                var dateFrom = Convert.ToDateTime(_filterDateFrom);
+
+                return dateFrom.ToString("yyyy-MM-ddT00:00:00Z");
+            }
+            set { _filterDateFrom = value; }
+        }
+
+        public string FilterDateTo
+        {
+            get
+            {
+                var dateTo = Convert.ToDateTime(_filterDateTo);
+
+                return dateTo.ToString("yyyy-MM-ddT23:59:59Z");
+            }
+            set { _filterDateTo = value; }
+        }
+
+
     }
 }
